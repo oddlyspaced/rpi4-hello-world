cd6c718f3045c15b70f2da6399128d26203babc5
oddlyspaced <oddlyspaced1@gmail.com>
2021-06-28 00:15:13 +0530

    src, include, Makefile: Import code from LLD and fix Makefile

---
+++ reference/include/peripherals/base.h
@@ -0,0 +1,3 @@
+#pragma once
+
+#define PBASE 0xFE000000
---
+++ reference/src/kernel.c
@@ -0,0 +1,9 @@
+#include "mini_uart.h"
+
+void kernel_main() {
+	uart_init();
+	uart_send_string("Hello World!\n");
+	while(1) {
+		uart_send(uart_recv());
+	}
+}
